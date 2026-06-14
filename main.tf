53ec1ba7e8321ea96e478002c69d1586287cf5fd
sasif6787 <asifarsh239@gmail.com>
2026-06-14 08:01:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0cae6d6fe6048ca2c"
+    image_id           = "ami-0521cb2d60cfbb1a6"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "minimajor"
+    key_name = "aachi"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-097b4975cf1130493", "subnet-01ee7e5094f09f366"]
+     subnets = ["subnet-027c5982dd0216138", "subnet-083ea579c8945392c"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
