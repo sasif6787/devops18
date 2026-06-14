4d7ea87ea024b26676c61540b258919e70f36135
sasif6787 <asifarsh239@gmail.com>
2026-06-14 08:03:12 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafashaik1122.monobucket"
+  bucket = "shaiku.terraform.flm.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -20,12 +20,5 @@ resource "aws_s3_bucket_versioning" "three" {
 bucket = aws_s3_bucket.one.id
 versioning_configuration {
 status = "Enabled"
-}
-}
-terraform {
-backend "s3" {
-region = "us-east-1"
-bucket = "mustafashaik1122.monobucket"
-key = "prod/terraform.tfstate"
 }
 }
